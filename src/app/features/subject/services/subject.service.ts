de12c4d882c2889c93f6415cfb86e61e0097895d
Leonardo Vučenović <101517924+leonardo-vucenovic@users.noreply.github.com>
2024-11-21 13:43:56 +0100

    Update subject.service.ts

--- src/app/features/subject/services/subject.service.ts
+++ src/app/features/subject/services/subject.service.ts
@@ -6,31 +6,18 @@ import { Subject } from '../models/subject.model';
 import { environment } from 'src/environments/environment';
 import { UpdateSubjectRequest } from '../models/update-subject-request.model';
 
-//Sa ovim govorimo da je service dostupan globalno cijeloj aplikaciji, odnosno svim komponentama
+
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectService {
 
-  //Kroz konstruktor ce Angular uz pomoc DI-a ubrizgati instancu pa ju ja ne moram stvarati ovdje
-  //HttpClient služi za razgovor sa backend-om za HTTP zahtijeve poput Post,Put,Get,Delete
+
+
   constructor(private http: HttpClient) { }
 
-  //Sve metode
 
-  //addSubject - Naziv metode, naziv preko kojeg pozovem radnju metode
-  //model: - Naziv za objekt kojeg primamo
-  //AddSubjectRequest - Tip podataka modela kojeg primamo
-  //: Observable<void> - Označava što metoda vraća, u ovom slučaju void pa ne vrača ništa i oznaka da je metoda Observable
-  create(model: AddSubjectRequest) : Observable<Subject> { //Backend vraca tog kreiranog Subjecta, možda kasnje sa voida promijeniti na Subject (dodatno promijeniti komponentu gdje se poziva ova metoda)
-    //Dodatno metoda mora biti Observable ako unutar nje koristim http jer ne znamo kada će backend vratiti odgovor
-    
-    //preko this.http pozovi post metodu na backendu i void označava da ništa ne dobivamo natrag od backenda
-    //to bi značilo da post<void> označava što metoda sa backenda vraća i tu može biti to da dobivam natrag Subject
-    //i onda nešto radim s njim ali zbog ovog gore void-a on označava povrat cijele metode
-    //i zbog toga na kraju moram vratiti void, ne ovisno o tome što metoda sa backenda vrača
-    //znači mogu dobiti natrag od backend-a Subject ali kad završi izvršavanje cijele metode na kraju moram vratiti void
-    //unutar zagrada je definiran endpoint kojeg gađamo da pogodimo metodu i model koji šaljemo
+  create(model: AddSubjectRequest) : Observable<Subject> {
     return this.http.post<Subject>(`${environment.apiBaseUrl}/Subjects/Create`, model)
   }
 
